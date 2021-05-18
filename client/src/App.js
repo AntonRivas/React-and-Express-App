7566273d6e3523864fd3db111c5dca0a8e7d99e2
AntonRivas <antondearribas@gmail.com>
2021-05-18 15:51:37 +0100

    Replaced Hooks with React Components

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,35 @@ import logo from './logo.svg';
 import React from 'react'
 //import './App.css';
 
-function App() {
-  const [word, setWord] = React.useState('software');
-  const [associations, setAssociations] = React.useState(null);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: "software",
+      associations: null
+    };
+  }
 
-  const getAssociations = () => {
-    fetch('/api/associations/' + word)
+  //const [word, setWord] = React.useState('software');
+  //const [associations, setAssociations] = React.useState(null);
+
+  getAssociations = () => {
+    console.log("This part ran")
+    fetch('/api/associations/' + this.state.word)
       .then(result => result.json())
-      .then(body => setAssociations(body));
+      .then(body => this.setState({associations: body}));
   };
-
+  render(){
   return (
     <div className="app">
       <h1>Word Associations Map</h1>
-      <input value={word} onChange={e => setWord(e.target.value)} />
-      <button onClick={getAssociations}>Find Associations</button>
-      {associations && (
-        Object.keys(associations).length === 0
+      <input value= {this.state.word} onChange = {e => this.setState({word:e.target.value})} />
+      <button onClick={this.getAssociations}>Find Associations</button>
+      {this.state.associations && (
+        Object.keys(this.state.associations).length === 0
           ? <p>No results</p>
           : <div>
-            {Object.entries(associations).map(([association, score]) => (
+            {Object.entries(this.state.associations).map(([association, score]) => (
               <span style={{ fontSize: Math.pow(score, 2) / 200 }}>
                 {association}
                 {' '}
@@ -30,7 +39,7 @@ function App() {
           </div>
       )}
     </div>
-  );
+  )};
 }
 
 export default App;
